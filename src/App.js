ae47bc744a51fca7c5ce9e6b13dc868f9cbc255c
Ilfir Yakupov <philsmirnoff@gmail.com>
2021-11-19 22:49:33 -0500

    created App.js and installed all packages

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <div className='navbar'>
+
+      </div>
+      <div className='main'>
+
+      </div>
+      <div classname='footer'>
+      </div>
+    </div>
+  )
+}
+
+export default App

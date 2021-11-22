6e521dbb2fd8cc82c5c07526cd5f86644c586c9d
Ilfir Yakupov <philsmirnoff@gmail.com>
2021-11-22 09:47:20 -0500

    website shows the top 10 cryptos in the world and all 50when click show more

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,13 +3,15 @@ import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import Cryptocurrencies from './Cryptocurrencies';
+import News from './News';
 
 
 const { Title } = Typography;
 
 
 const Homepage = () => {
-  const { data, isFetching } = useGetCryptosQuery();
+  const { data, isFetching } = useGetCryptosQuery(10);
   const globalStats = data?.data?.stats;
   // console.log(data);
 
@@ -25,6 +27,16 @@ const Homepage = () => {
         <Col span={12}><Statistic title='Total 24h Volume' value={`$${millify(globalStats.total24hVolume)}`} /></Col>
         <Col span={12}><Statistic title='Total Markets' value={millify(globalStats.totalMarkets)} /></Col>
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Top 10 Cryptos In The World</Title>
+        <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show more</Link></Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Latest Crypto News</Title>
+        <Title level={3}><Link to="/news">Show more</Link></Title>
+      </div>
+      <News simplified />
     </>
   )
 }

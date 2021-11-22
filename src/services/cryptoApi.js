0baf057f76b0da86381e73a1d799fce89c9f38dd
Ilfir Yakupov <philsmirnoff@gmail.com>
2021-11-22 00:44:45 -0500

    created store and imported Provider into index.js

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,9 @@
+
+// var options = {
+//   method: 'GET',
+//   url: 'https://coinranking1.p.rapidapi.com/exchanges',
+//   headers: {
+//     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//     'x-rapidapi-key': '7bed659c00msh43a443c538d73f0p1d7345jsn230e38afb912'
+//   }
+// };

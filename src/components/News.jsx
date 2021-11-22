85a57f4420152bd1b542e3e2c3131a473842613d
Ilfir Yakupov <philsmirnoff@gmail.com>
2021-11-22 12:11:28 -0500

    fetched the data from CryproNews Api

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
+import moment from 'moment';
+import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 
-const News = () => {
+const { Text, Title } = Typography;
+const { Option } = Select;
+
+const News = ({ simplified }) => {
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 10 : 100 })
+
+  if (!cryptoNews?.value) return 'Loading...';
   return (
     <div>
    News

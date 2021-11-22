b8fbfe22d435abed66be407a4906d5d1128c46d5
Ilfir Yakupov <philsmirnoff@gmail.com>
2021-11-22 00:27:32 -0500

    created Clobal Crypto Stats in Homepage component

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,11 +3,20 @@ import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
+const { Title } = Typography;
+
 const Homepage = () => {
   return (
-    <div>
-      Homepage
-    </div>
+    <>
+      <Title level={2} className='heading'>Global Crypto Stats</Title>
+      <Row>
+      <Col span={12}><Statistic title='Total Cryptocurrencies' /></Col>
+        <Col span={12}><Statistic title='Total Exchanges'  /></Col>
+        <Col span={12}><Statistic title='Total Market Cap:' /></Col>
+        <Col span={12}><Statistic title='Total 24h Volume' /></Col>
+        <Col span={12}><Statistic title='Total Markets' /></Col>
+      </Row>
+    </>
   )
 }
 

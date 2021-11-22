80c5a106ce430997dbaf7ac74d54c56e3659a255
Ilfir Yakupov <philsmirnoff@gmail.com>
2021-11-21 23:07:49 -0500

    created routes in App.js for components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 import Navbar from './components/Navbar';
+import Exchanges from './components/Exchanges';
+import Homepage from './components/Homepage';
+import Cryptocurrencies from './components/Cryptocurrencies';
+import News from './components/News';
+import CryptoDetails from './components/CryptoDetails';
 import './App.css'
 
 
@@ -10,10 +15,29 @@ const App = () => {
     <div className='app'>
       <div className='navbar'>
         <Navbar />
-
       </div>
       <div className='main'>
-
+        <Layout>
+          <div classname='routes'>
+            <Switch>
+              <Route exact path='/'>
+                <Homepage />
+              </Route>
+              <Route exact path='/exchanges'>
+                <Exchanges />
+              </Route>
+              <Route exact path='/cryptocurrencies'>
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path='/crypto/:coinId'>
+                <CryptoDetails />
+              </Route>
+              <Route exact path='/news'>
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
       </div>
       <div classname='footer'>
       </div>

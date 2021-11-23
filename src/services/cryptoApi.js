0e7eed9c821ae062b8b02424dbd04fb9c3a3d8b8
Ilfir Yakupov <philsmirnoff@gmail.com>
2021-11-23 00:28:36 -0500

    added Excanges component

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -25,6 +25,9 @@ export const cryptoApi = createApi({
     getCryptos: builder.query({
       query: (count) => createRequest(`/coins?limit=${count}`),
     }),
+    getExchanges: builder.query({
+      query: () => createRequest('/exchanges'),
+    }),
     getCryptoDetails: builder.query({
       query: (coinId) => createRequest(`/coin/${coinId}`),
     }),
@@ -35,6 +38,6 @@ export const cryptoApi = createApi({
 });
 
 export const {
-  useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery
+  useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery, useGetExchangesQuery
   // Redux toolkit create the hook to fetch the data to all queryies
 } = cryptoApi;

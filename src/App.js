84145ce4debd16f6696dfc176d7ba7e4fa987332
Ilfir Yakupov <philsmirnoff@gmail.com>
2025-07-23 15:15:01 -0400

    clean up

--- src/App.js
+++ src/App.js
@@ -1,58 +1,45 @@
-import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
-import { Layout, Typography, Space } from 'antd';
-import Navbar from './components/Navbar';
-import Homepage from './components/Homepage';
-import Cryptocurrencies from './components/Cryptocurrencies';
-import News from './components/News';
-import CryptoDetails from './components/CryptoDetails';
-import CryptoQuiz from './components/CryptoQuiz';
-import './App.css'
-
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import Navbar from "./components/Navbar";
+import Homepage from "./components/Homepage";
+import Cryptocurrencies from "./components/Cryptocurrencies";
+import News from "./components/News";
+import CryptoDetails from "./components/CryptoDetails";
+import CryptoQuiz from "./components/CryptoQuiz";
+import "./App.css";
 
 const App = () => {
   return (
-    <div className='app'>
-      <div className='navbar'>
+    <div className="app">
+      <div className="navbar">
         <Navbar />
       </div>
-      <div className='main'>
+      <div className="main">
         <Layout>
-          <div className='routes'>
+          <div className="routes">
             <Switch>
-              <Route exact path='/'>
+              <Route exact path="/">
                 <Homepage />
               </Route>
-              <Route exact path='/cryptocurrencies'>
+              <Route exact path="/cryptocurrencies">
                 <Cryptocurrencies />
               </Route>
-              <Route exact path='/crypto/:coinId'>
+              <Route exact path="/crypto/:coinId">
                 <CryptoDetails />
               </Route>
-              <Route exact path='/news'>
+              <Route exact path="/news">
                 <News />
               </Route>
-              <Route exact path='/cryptoquiz'>
+              <Route exact path="/cryptoquiz">
                 <CryptoQuiz />
               </Route>
             </Switch>
           </div>
         </Layout>
-      <div className='footer'>
-        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center'}}>
-          CryptoMaster <br />
-          All rights reserved
-        </Typography.Title>
-        <Space>
-          <Link to='/'>Home</Link>
-          <Link to='/cryptocurrencies'>Cryptocurrencies</Link>
-          <Link to='/news'>News</Link>
-          <Link to='/cryptoquiz'>CryptoQuiz</Link>
-        </Space>
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;

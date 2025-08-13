ad82f7d62e6baf0ea9269cbe99a403b6a077589a
Ilfir Yakupov <philsmirnoff@gmail.com>
2025-08-13 01:54:48 -0400

    add close  burger menu on click

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Menu, Typography, Avatar } from 'antd';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 
 import icon from '../images/crypto.png';
@@ -8,6 +8,7 @@ import icon from '../images/crypto.png';
 const Navbar = () => {
   const [activeMenu, setActiveMenu] = useState(true);
   const [screenSize, setScreenSize] = useState(undefined);
+  const location = useLocation();
 
   useEffect(() => {
     const handleResize = () => setScreenSize(window.innerWidth);
@@ -27,6 +28,19 @@ const Navbar = () => {
     }
   }, [screenSize]);
 
+  // Close mobile menu when route changes
+  useEffect(() => {
+    if (screenSize <= 800) {
+      setActiveMenu(false);
+    }
+  }, [location.pathname, screenSize]);
+
+  const handleMenuClick = () => {
+    if (screenSize <= 800) {
+      setActiveMenu(false);
+    }
+  };
+
   return (
     <div className="nav-container">
       <div className="logo-container">
@@ -37,16 +51,16 @@ const Navbar = () => {
       {activeMenu && (
       <Menu theme="dark">
         <Menu.Item icon={<HomeOutlined />}>
-          <Link to="/">Home</Link>
+          <Link to="/" onClick={handleMenuClick}>Home</Link>
         </Menu.Item>
         <Menu.Item icon={<FundOutlined />}>
-          <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+          <Link to="/cryptocurrencies" onClick={handleMenuClick}>Cryptocurrencies</Link>
         </Menu.Item>
         <Menu.Item icon={<BulbOutlined />}>
-          <Link to="/news">News</Link>
+          <Link to="/news" onClick={handleMenuClick}>News</Link>
         </Menu.Item>
         <Menu.Item icon={<MoneyCollectOutlined />}>
-          <Link to="/cryptoquiz">CryptoQuiz</Link>
+          <Link to="/cryptoquiz" onClick={handleMenuClick}>CryptoQuiz</Link>
         </Menu.Item>
       </Menu>
       )}
